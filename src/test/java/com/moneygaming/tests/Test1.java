4c920c4844a8e0b9364846afa3fdda7dbcfd8928
ibrahimkuralozkan <ibrahimkuralozkan@gmail.com>
2021-11-16 00:43:33 +0100

    first part of project

--- src/test/java/com/moneygaming/tests/Test1.java
+++ src/test/java/com/moneygaming/tests/Test1.java
@@ -1,19 +1,22 @@
 package com.moneygaming.tests;
 
 import com.moneygaming.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Test1 {
-
+    WebDriver  driver = WebDriverFactory.getDriver("chrome");
     @BeforeMethod
     public void setuo(){
 
-        WebDriver  driver = WebDriverFactory.getDriver("chrome");
+
         driver.manage().window().maximize();
+        driver.get("https://moneygaming.qa.gameaccount.com/sign-up.shtml");
 
 
 
@@ -24,18 +27,20 @@ public class Test1 {
 
 
     @Test
-    public void  test1(){
+    public void  test1() throws InterruptedException {
 
-
-
+        WebElement firstname= driver.findElement(By.id("forename"));
+        firstname.sendKeys("ibrahim");
+        WebElement sirname= driver.findElement(By.cssSelector("input[name='map(lastName)']"));
+        sirname.sendKeys("krl");
+        Thread.sleep(2000);
 
     }
 
     @AfterMethod
     public void tearDown(){
 
-
-
+driver.close();
     }
 
 
